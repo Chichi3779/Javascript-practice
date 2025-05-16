6fb8054521911055d2e6f3620ac1ec4b43734593
Chichi3779 <johnrosey4@gmail.com>
2025-05-16 09:46:43 +0100

    my recent tasks on objects and variable declaration

---
+++ index.js
@@ -0,0 +1,63 @@
+
+// TASK FOR 14TH MAY
+
+let numbers = [ 1, 5, 7, 6, 8, 9];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+}
+    console.log(sum);
+
+
+//For loop
+// let num = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10];
+// for(let i = 0; i < num.length; i++){
+//     if(num[i] % 2 == 0){
+//         console.log(num[i] + " is even ");
+//     } else{
+//         console.log(num[i] + " is odd ");
+//     }
+// }
+
+//while loop
+// let num = 5;
+// while (num >= 0){
+//     console.log(num);
+//     num--;
+// }
+
+
+//do...while loop
+// let name;
+// do{
+//     name = prompt("Enter your name:")
+// } while(name == "");
+
+
+//forEach loop
+// let colors = ["red", "blue", "pink", "white"];
+
+// colors.forEach(function(color, index){
+//     console.log("Color " + (index + 1) + " is " + color);
+// });
+
+//ARRAY METHODS
+// let fruits = ["apple"];
+// fruits.push("Banana");
+// fruits.shift();
+// fruits.unshift("orange","pear","coconut")
+// fruits.splice(1, 2, "pawpaw")
+
+// console.log(fruits)
+
+
+//to print a sentence and print it in upper case
+
+// let name = "chiCHIN   ";
+
+// console.log(name.length);
+// console.log(name.trim());
+// console.log(name.replace("chi","Damian"));
+// console.log(name.toUpperCase());
+
---
+++ object.js
@@ -0,0 +1,62 @@
+//  let book = {
+//     author: "Mark Benson",
+//     title: "THE BOY NAMED KUNLE",
+//     yearPublished: 2023,
+//     isAvailable : true,
+
+//     borrowBook: function() {
+//         return isAvailable = false;
+//     }
+//  }
+
+//  console.log(book.author);
+//   console.log(book["title"]);
+//    console.log(book.year);
+
+//  let user = {
+//     name: "Mark Zuks",
+//     email: "johnrosey4@gmail.com",
+//     age: 20,
+//     //   Talents
+//     borrowBook: function() {
+//         return isAvailable = true;
+//         isAvailable = false;
+//     }
+//  }
+
+//  console.log("My name is " + user.name);
+//  console.log(user["email"]);
+//  console.log("I am " + user.age + " Years old.");
+//  console.log(user.talent());
+
+// console.log(book.borrowBook());
+// console.log(book.isAvailable);
+
+
+//  let person = {
+//     name: "Mark Benson",
+//     city: "New york",
+//     age: 20,
+    
+//     //A  method to introduce yourself
+//     introduce: function() {
+//         return "Hi, I'm " + person.name + ", " + person.age + " years old, and I live in " + person.city;
+//     }
+//  }
+
+//  console.log(person.introduce());
+
+
+let firstName = "Chinenye";
+let lastName = "Ndubuisi";
+let age = 20;
+let driverLicense = false;
+let dreamJob;
+dreamJob = "Full Stack developer";
+
+ console.log(firstName);
+ console.log(lastName);
+ console.log(age);
+ console.log(driverLicense);
+ console.log(dreamJob);
+
